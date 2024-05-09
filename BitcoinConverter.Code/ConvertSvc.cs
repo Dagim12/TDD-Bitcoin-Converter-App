3d3120a186afa06b42d61df046df14c47638db60
Dagim Belayneh <dbelayneh@excellerentsolutions.com>
2024-05-09 18:49:38 +0300

    added convert to currency and theory test method

--- BitcoinConverter.Code/ConvertSvc.cs
+++ BitcoinConverter.Code/ConvertSvc.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading.Tasks;
 
 namespace Technopia.BitcoinConverter
 {
@@ -8,10 +9,25 @@ namespace Technopia.BitcoinConverter
 
         }
 
-        public GetExchangeRate(string currency){
-            if(currency == "USD"){
+        public int GetExchangeRate(string currency) {
+            if(currency.Equals("USD")){
                 return 100;
             }
+            else if(currency.Equals("GBP")){
+                return 200;
+            }
+            else if(currency.Equals("EUR")){
+                return 150;
+            }
+            return 0;
         }
+
+        public async Task<double> ConvertBitcoins(string currency, double coins)
+        {
+            var exchangeRate = GetExchangeRate(currency);
+
+            return exchangeRate*coins;
+        }
+
     }
 }
--- BitcoinConverter.Tests/BitCoinConvetetShould.cs
+++ BitcoinConverter.Tests/BitCoinConvetetShould.cs
@@ -1,9 +1,10 @@
 using System;
+using System.Threading.Tasks;
 using Xunit;
 
 namespace Technopia.BitcoinConverter.Tests
 {
-    public class BitCoinConvetetShould
+    public class BitCoinConveterShould
     {
         [Fact]
         public void GetExchangeRate_USD_ReturnsUSDExchangeRate()
@@ -17,6 +18,50 @@ namespace Technopia.BitcoinConverter.Tests
             //Assert
             var expectedRate = 100;
             Assert.Equal(expectedRate,exchangeRate);
+        }
+        [Fact]
+        public void GetExchangeRate_EUR_ReturnsUSDExchangeRate()
+        {
+            //Arange
+            var conveterSvc = new ConvertSvc();
+
+            //Act
+            var exchangeRate = conveterSvc.GetExchangeRate("EUR");
+
+            //Assert
+            var expectedRate = 150;
+            Assert.Equal(expectedRate,exchangeRate);
+        }
+
+        [Fact]
+        public void GetExchangeRate_GBP_ReturnsGBPExchangeRate()
+        {
+            //Arange
+            var conveterSvc = new ConvertSvc();
+
+            //Act
+            var exchangeRate = conveterSvc.GetExchangeRate("GBP");
+
+            //Assert
+            var expectedRate = 200;
+            Assert.Equal(expectedRate,exchangeRate);
+        }
+
+        [Theory]
+        [InlineData("USD",1,100)]
+        [InlineData("USD",2,200)]
+        [InlineData("EUR",1,150)]
+        [InlineData("EUR",2,300)]
+        [InlineData("GBP",1,200)]
+        [InlineData("GBP",2,400)]
+        public async Task ConvertBitcoins_BitCoinToCurrency_ReturnsCurrency(string currency, int coins, int convertedCurrency)
+        {
+            // Given
+            var conveterSvc = new ConvertSvc();
+            // When
+            var converted = await conveterSvc.ConvertBitcoins(currency,coins);
+            // Then
+            Assert.Equal(convertedCurrency,converted);
         }
     }
 }
